f3b5161126e797746826164d645578b0d9741804
Adam Shimali <adam.shimali@gmail.com>
2015-07-08 15:53:40 +0100

    post to index redirect to full check url or result

--- check/frontend/views.py
+++ check/frontend/views.py
@@ -22,24 +22,34 @@ frontend = Blueprint('frontend', __name__, template_folder='templates')
 def index():
     form = CheckForm()
     if form.validate_on_submit():
-        register, key = form.data['register_id'].split(':')
         try:
-            url = _get_url(register, key)
-            resp = requests.get(url, headers=headers)
-            if resp.status_code == 200:
-                entry = resp.json()
-                address = _get_address(entry)
-                return render_template('check.html', entry=entry, address=address)
-            else:
-                message = "There was a problem checking the %s register" % register
-                flash(message)
-                abort(resp.status_code)
+            register, key = form.data['register_id'].split(':')
+            return redirect(url_for('frontend.check', register=register, key=key))
         except KeyError as e:
             message = "This application is not yet configured to check the %s register" % register
             flash(message)
             return redirect(url_for('.index'))
-    else:
-        return render_template('index.html', form=form)
+
+    return render_template('index.html', form=form)
+
+
+@frontend.route('/check/<register>/<key>')
+def check(register, key):
+    try:
+        url = _get_url(register, key)
+        resp = requests.get(url, headers=headers)
+        if resp.status_code == 200:
+            entry = resp.json()
+            address = _get_address(entry)
+            return render_template('check.html', entry=entry, address=address)
+        else:
+            message = "There was a problem checking the %s register" % register
+            flash(message)
+            abort(resp.status_code)
+    except KeyError as e:
+        message = "This application is not yet configured to check the %sregister" % register
+        flash(message)
+    return redirect(url_for('.index'))
 
 
 def _get_url(register, key):
